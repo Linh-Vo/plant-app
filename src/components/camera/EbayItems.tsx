f03c777d5d5fa7fff0e102ba9594e6e4d14b9f6a
Linh Vo <linh@y.digital>
2022-06-30 14:16:21 +0700

    split ebay + wiki info

---
+++ src/components/camera/EbayItems.tsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, ScrollView, Linking, ActivityIndicator} from 'react-native';
+import {TextStyle} from '../../styles/base';
+import {EbayItems} from '../../types';
+import Touchable from '../../components/CustomTouchable';
+import axios from 'axios';
+import {API_URL} from 'react-native-dotenv';
+import {theme} from '../../theme/theme';
+
+const EbayItem = ({plantName}) => {
+  const [ebayItems, setItems] = useState<EbayItems[] | []>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get('http://192.168.1.3:8088/ebay-items', {
+        params: {
+          plant_name: plantName,
+        },
+      })
+      .then(res => {
+        setLoading(false);
+        const data = res.data;
+        const items =
+          (data?.findItemsAdvancedResponse &&
+            data?.findItemsAdvancedResponse[0]?.searchResult[0]?.item) ||
+          [];
+        setItems(items);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [plantName]);
+  if (loading) {
+    return <ActivityIndicator size={'small'} color={theme.color.primary} />;
+  }
+  return ebayItems?.length > 0 ? (
+    <View style={{marginTop: theme.spacing.triple}}>
+      <Text
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{...TextStyle.bodyText, opacity: 0.6}}>
+        {'Where to buy'}
+      </Text>
+      <ScrollView
+        style={{marginTop: theme.spacing.double}}
+        showsVerticalScrollIndicator={false}>
+        {ebayItems.map((it, idx) => (
+          <Touchable
+            key={idx}
+            onPress={() => Linking.openURL(`${it.viewItemURL[0]}`)}
+            it={it}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  ) : null;
+};
+
+export default EbayItem;
---
+++ src/components/camera/WikiContent.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, Linking, ActivityIndicator} from 'react-native';
+import {TextStyle} from '../../styles/base';
+import {PlantWikiInfo} from '../../types';
+import axios from 'axios';
+import {API_URL} from 'react-native-dotenv';
+import {theme} from '../../theme/theme';
+
+const WikiItem = ({plantName}) => {
+  const [wikiInfo, setWikiInfo] = useState<PlantWikiInfo | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get('http://192.168.1.3:8088/wiki-info', {
+        params: {
+          plant_name: plantName,
+        },
+      })
+      .then(res => {
+        setLoading(false);
+        setWikiInfo(res.data);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [plantName]);
+  if (loading) {
+    return (
+      <View style={{marginTop: theme.spacing.triple}}>
+        <ActivityIndicator size={'small'} color={theme.color.primary} />
+      </View>
+    );
+  }
+  console.log(API_URL);
+  return wikiInfo?.description || wikiInfo?.introduction ? (
+    <View style={{marginTop: theme.spacing.triple}}>
+      <Text
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{...TextStyle.bodyText, opacity: 0.6}}>
+        {'Description'}
+      </Text>
+      <Text
+        style={{
+          ...TextStyle.titleText,
+          marginTop: theme.spacing.base,
+        }}>
+        {(wikiInfo?.description?.length && wikiInfo?.description?.join()) ||
+          wikiInfo?.introduction}
+      </Text>
+      <Text
+        style={{
+          ...TextStyle.titleText,
+          color: theme.color.primary,
+          marginTop: theme.spacing.base,
+        }}
+        onPress={() =>
+          Linking.openURL(`https://en.wikipedia.org/wiki/${plantName}`)
+        }>
+        {'Read more on Wikipedia'}
+      </Text>
+    </View>
+  ) : null;
+};
+
+export default WikiItem;
